2b647a5772c64de7b678171c405fe57cc8706943
Ticher <magma2003@gmail.com>
2022-11-19 20:52:10 -0300

    third commit

--- src/main/java/com/egg/news/entities/Periodista.java
+++ src/main/java/com/egg/news/entities/Periodista.java
@@ -3,19 +3,22 @@ package com.egg.news.entities;
 import java.util.ArrayList;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
 import javax.persistence.ManyToOne;
 
-import org.hibernate.annotations.GenericGenerator;
-
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 @AllArgsConstructor
-public class Periodista extends Usuario{
+public class Periodista extends Usuario {
     @ManyToOne
     private ArrayList<Noticia> misNoticias;
     private Integer sueldoMensual;
